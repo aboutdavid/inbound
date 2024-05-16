c27d5602e086741da8254e254a9f9fe5a8521356
David M <62346025+aboutdavid@users.noreply.github.com>
2024-05-16 09:57:29 -0400

    add estimated flight + late/early

--- index.js
+++ index.js
@@ -13,92 +13,97 @@ const app = new App({
 });
 
 (async () => {
-async function pull(){
-  config.events.forEach(async event => {
-    if (new Date(event.start) > new Date() || new Date() > new Date(event.end)) return 
+  async function pull() {
+    config.events.forEach(async event => {
+      if (new Date(event.start) > new Date() || new Date() > new Date(event.end)) return
 
 
-    const data = await app.client.conversations.history({
-      channel: event.channel,
-    })
-    const { messages } = data
-
-    await Promise.all(
-      messages.map((message) =>
-        app.client.chat
-          .delete({
-            token: process.env.SLACK_USER_TOKEN, // sudo
-            channel: event.channel,
-            ts: message?.ts,
-            thread_ts: message?.thread_ts,
-          })
-          .catch((e) => {
-            console.warn(e)
-          })
-      ))
-
-
-    let promises = event.users.map(async user => {
-
-      var flights = await Promise.all(user.flights.map(async flight => {
-        const res = await utils.checkFlight(flight.code, new Date(flight.date));
-        if (!res.error) return res
-      }))
-      flights = flights.sort(function (a, b) {
-
-        return new Date(a.scheduled_out) - new Date(b.scheduled_out);
-      });
-
-      const flightsLeft = flights.filter(flight => !flight.actual_in).length
-      var txt = ""
-
-
-      txt += flightsLeft == 0 ? `${user.name} has landed from their final flight.` : `<@${user.id}> has ${flightsLeft} ${flightsLeft != 1 ? "flights" : "flight"} to complete`;
-
-      flights.forEach((flight, i) => {
-        txt += `\n\n*${flight.ident_iata}: ${flight.origin?.code_iata} -> ${flight.destination?.code_iata}*\n`
-        txt += `ℹ️ Status: ${flight.status} | 🛄 Baggage claim: ${flight.baggage_claim || "None"} | Gate at ${flight.origin?.code_iata}: ${flight.gate_origin} | Gate at ${flight.destination?.code_iata}: ${flight.gate_destination} \n`
-        txt += `🛫 Take off (scheduled): ${flight.scheduled_off ? new Date(flight.scheduled_off).toLocaleString('en-US', { timeZone: event.timezone, timeStyle: "short", dateStyle: "long" }) : "Unknown"} `
-        txt += `(Actual: ${flight.actual_off ? new Date(flight.actual_off).toLocaleString('en-US', { timeZone: event.timezone, timeStyle: "short", dateStyle: "long" }) : "Unknown"})\n`
-        txt += `🛬 Landing (scheduled): ${flight.scheduled_in ? new Date(flight.scheduled_in).toLocaleString('en-US', { timeZone: event.timezone, timeStyle: "short", dateStyle: "long" }) : "Unknown"} `
-        txt += `(Actual: ${flight.actual_in ? new Date(flight.actual_in).toLocaleString('en-US', { timeZone: event.timezone, timeStyle: "short", dateStyle: "long" }) : "Unknown"})\n`
-        txt += `Progress: (${flight.progress_percent}%) ${utils.progress(flight.progress_percent)}\n`
-
-      })
-
-      return txt
-    });
-
-    let texts = await Promise.all(promises);
-    texts.push(`⌚ All times are in ${event.timezone}`)
-    let text = texts.join('\n');
-
-    let chunks = [];
-    let i = 0;
-    while (i < text.length) {
-      let end = Math.min(i + 40000, text.length);
-      if (end < text.length) {
-        end = text.lastIndexOf('\n', end);
-        if (end == -1) {
-          end = Math.min(i + 40000, text.length);
-        }
-      }
-      chunks.push(text.substring(i, end));
-      i = end;
-    }
-
-    for (let chunk of chunks) {
-      await app.client.chat.postMessage({
+      const data = await app.client.conversations.history({
         channel: event.channel,
-        text: chunk,
-        mrkdwn: true
-      });
-    }
-  })
-}
+      })
+      const { messages } = data
 
-await pull()
-setTimeout(pull, 1000 * 60 * 5)
+      await Promise.all(
+        messages.map((message) =>
+          app.client.chat
+            .delete({
+              token: process.env.SLACK_USER_TOKEN, // sudo
+              channel: event.channel,
+              ts: message?.ts,
+              thread_ts: message?.thread_ts,
+            })
+            .catch((e) => {
+              console.warn(e)
+            })
+        ))
+
+
+      let promises = event.users.map(async user => {
+
+        var flights = await Promise.all(user.flights.map(async flight => {
+          const res = await utils.checkFlight(flight.code, new Date(flight.date));
+          if (!res.error) return res
+        }))
+        flights = flights.sort(function (a, b) {
+
+          return new Date(a.scheduled_out) - new Date(b.scheduled_out);
+        });
+
+        const flightsLeft = flights.filter(flight => !flight.actual_in).length
+        var txt = ""
+
+
+        txt += flightsLeft == 0 ? `${user.name} has landed from their final flight.` : `<@${user.id}> has ${flightsLeft} ${flightsLeft != 1 ? "flights" : "flight"} to complete`;
+
+        flights.forEach((flight, i) => {
+          txt += `\n\n*${flight.ident_iata}: ${flight.origin?.code_iata} -> ${flight.destination?.code_iata}*\n`
+          txt += `ℹ️ Status: ${flight.status} | 🛄 Baggage claim: ${flight.baggage_claim || "None"} | Gate at ${flight.origin?.code_iata}: ${flight.gate_origin} | Gate at ${flight.destination?.code_iata}: ${flight.gate_destination} \n`
+          txt += `🛫 Take off (scheduled): ${flight.scheduled_off ? utils.fmtDate(flight.scheduled_off, event.timezone) : "Unknown"} `
+          txt += `(Estimated: ${flight.actual_off ? utils.fmtDate(flight.estimated_off, event.timezone) : "Unknown"}) `
+          txt += `(Actual: ${flight.actual_off ? utils.fmtDate(flight.actual_off, event.timezone) : "Unknown"})\n`
+          if (flight.scheduled_off && flight.actual_off) txt += `_${utils.humanReadableDiff(flight.scheduled_off, flight.actual_off)}_\n`
+          txt += `🛬 Landing (scheduled): ${flight.scheduled_in ? utils.fmtDate(flight.scheduled_in, event.timezone) : "Unknown"} `
+          txt += `(Estimated: ${flight.estimated_in_in ? utils.fmtDate(flight.estimated_in, event.timezone) : "Unknown"}) `
+          txt += `(Actual: ${flight.actual_in ? utils.fmtDate(flight.actual_in, event.timezone) : "Unknown"})\n`
+          if (flight.scheduled_in && flight.actual_in) txt += `_${utils.humanReadableDiff(flight.scheduled_in, flight.actual_in)}_\n`
+
+          txt += `Progress: (${flight.progress_percent}%) ${utils.progress(flight.progress_percent)}\n`
+
+        })
+
+        return txt
+      });
+
+      let texts = await Promise.all(promises);
+      texts.push(`⌚ All times are in ${event.timezone}`)
+      let text = texts.join('\n');
+
+      let chunks = [];
+      let i = 0;
+      while (i < text.length) {
+        let end = Math.min(i + 40000, text.length);
+        if (end < text.length) {
+          end = text.lastIndexOf('\n', end);
+          if (end == -1) {
+            end = Math.min(i + 40000, text.length);
+          }
+        }
+        chunks.push(text.substring(i, end));
+        i = end;
+      }
+
+      for (let chunk of chunks) {
+        await app.client.chat.postMessage({
+          channel: event.channel,
+          text: chunk,
+          mrkdwn: true
+        });
+      }
+    })
+  }
+
+  await pull()
+  setTimeout(pull, 1000 * 60 * 5)
 
 
   await app.start(process.env.PORT || 3000);
--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const reachedDateLimit = (date) => (date - Date.now()) > 2 * 24 * 60 * 60 * 1000 || (Date.now() - date) > 8 * 24 * 60 * 60 * 1000;
 
 async function checkFlight(code, date) {
-    if (reachedDateLimit(date)) return { error: true}
+    if (reachedDateLimit(date)) return { error: true }
     if (!/^[A-Za-z]{2}\d{1,4}$/.test(code)) return { invalid: true }
     const res = await fetch(`https://aeroapi.flightaware.com/aeroapi/flights/${code}?` + new URLSearchParams({
         start: new Date(date).toISOString(),
@@ -30,4 +30,34 @@ function progress(int) {
     bar += filled + empty + "]";
     return bar;
 }
-module.exports = { checkFlight, progress }
+
+function fmtDate(date, tz) {
+    return new Date(date).toLocaleString('en-US', { timeZone: tz, timeStyle: "short", dateStyle: "long" })
+}
+
+function humanReadableDiff(date1, date2) {
+    date1 = new Date(date1)
+    date2 = new Date(date2)
+    var seconds = Math.floor((date2 - date1) / 1000);
+    var prefix = seconds < 0 ? " early" : " late";
+    seconds = Math.abs(seconds);
+
+    var interval = Math.floor(seconds / 31536000);
+    if (interval >= 1) return interval + " year" + (interval > 1 ? "s" : "") + prefix;
+
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) return interval + " month" + (interval > 1 ? "s" : "") + prefix;
+
+    interval = Math.floor(seconds / 86400);
+    if (interval >= 1) return interval + " day" + (interval > 1 ? "s" : "") + prefix;
+
+    interval = Math.floor(seconds / 3600);
+    if (interval >= 1) return interval + " hour" + (interval > 1 ? "s" : "") + prefix;
+
+    interval = Math.floor(seconds / 60);
+    if (interval >= 1) return interval + " minute" + (interval > 1 ? "s" : "") + prefix;
+
+    return Math.floor(seconds) + " second(s)" + prefix;
+}
+
+module.exports = { checkFlight, progress, fmtDate, humanReadableDiff }
