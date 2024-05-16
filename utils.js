32af078470bf8d97a43b0ba941f56649f5432500
David M <62346025+aboutdavid@users.noreply.github.com>
2024-05-16 09:57:42 -0400

    add utils update

--- utils.js
+++ utils.js
@@ -44,16 +44,12 @@ function humanReadableDiff(date1, date2) {
 
     var interval = Math.floor(seconds / 31536000);
     if (interval >= 1) return interval + " year" + (interval > 1 ? "s" : "") + prefix;
-
     interval = Math.floor(seconds / 2592000);
     if (interval >= 1) return interval + " month" + (interval > 1 ? "s" : "") + prefix;
-
     interval = Math.floor(seconds / 86400);
     if (interval >= 1) return interval + " day" + (interval > 1 ? "s" : "") + prefix;
-
     interval = Math.floor(seconds / 3600);
     if (interval >= 1) return interval + " hour" + (interval > 1 ? "s" : "") + prefix;
-
     interval = Math.floor(seconds / 60);
     if (interval >= 1) return interval + " minute" + (interval > 1 ? "s" : "") + prefix;
 
